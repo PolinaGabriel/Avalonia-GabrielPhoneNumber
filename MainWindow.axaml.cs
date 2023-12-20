be5a7743c20b8472eecf3487b5418ced015c01bb
PolinaGabriel <114815179+PolinaGabriel@users.noreply.github.com>
2023-12-20 18:13:29 +0300

    Update MainWindow.axaml.cs

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -27,6 +27,27 @@ namespace GabrielPhoneNumber
 				phoneInt = 99999999999;
 			}
 			string phoneString = Convert.ToString(phoneInt);
+			Format(phoneString);
+		}
+
+		/// <summary>
+		/// Уменьшение числа в текстблоке на 1
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="rea1"></param>
+		void PhoneMinus(object? sender, RoutedEventArgs rea1)
+        {
+			long phoneInt = Convert.ToInt32(phone.Text.Substring(1, 1) + phone.Text.Substring(4, 3) + phone.Text.Substring(9, 2) + phone.Text.Substring(12, 2) + phone.Text.Substring(15)) - 1;
+			if (phoneInt < 0)
+            {
+                phoneInt = 0;
+            }
+            string phoneString = Convert.ToString(phoneInt);
+			Format(phoneString);
+        }
+
+		void Format(string phoneString)
+		{
 			switch (phoneString.Length)
 			{
 				case 1:
@@ -73,67 +94,6 @@ namespace GabrielPhoneNumber
 					phone.Text = "+" + phoneString.Substring(0, 1) + " (" + phoneString.Substring(1, 3) + ") " + phoneString.Substring(4, 2) + "-" + phoneString.Substring(5, 2) + "-" + phoneString.Substring(7);
 					break;
 			}
-        }
-
-		/// <summary>
-		/// Уменьшение числа в текстблоке на 1
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="rea1"></param>
-		void PhoneMinus(object? sender, RoutedEventArgs rea1)
-        {
-			long phoneInt = Convert.ToInt32(phone.Text.Substring(1, 1) + phone.Text.Substring(4, 3) + phone.Text.Substring(9, 2) + phone.Text.Substring(12, 2) + phone.Text.Substring(15)) - 1;
-			if (phoneInt < 0)
-            {
-                phoneInt = 0;
-            }
-            string phoneString = Convert.ToString(phoneInt);
-            switch (phoneString.Length)
-            {
-				case 1:
-					phone.Text = "+0 (000) 00-00-00" + phoneString;
-					break;
-
-				case 2:
-					phone.Text = "+0 (000) 00-00-0" + phoneString;
-					break;
-
-				case 3:
-					phone.Text = "+0 (000) 00-00-" + phoneString;
-					break;
-
-				case 4:
-					phone.Text = "+0 (000) 00-0" + phoneString.Substring(0, 1) + "-" + phoneString.Substring(1);
-					break;
-
-				case 5:
-					phone.Text = "+0 (000) 00-" + phoneString.Substring(0, 2) + "-" + phoneString.Substring(2);
-					break;
-
-				case 6:
-					phone.Text = "+0 (000) 0" + phoneString.Substring(0, 1) + "-" + phoneString.Substring(1, 2) + "-" + phoneString.Substring(3);
-					break;
-
-				case 7:
-					phone.Text = "+0 (000) " + phoneString.Substring(0, 2) + "-" + phoneString.Substring(2, 2) + "-" + phoneString.Substring(4);
-					break;
-
-				case 8:
-					phone.Text = "+0 (00" + phoneString.Substring(0, 1) + ") " + phoneString.Substring(1, 2) + "-" + phoneString.Substring(3, 2) + "-" + phoneString.Substring(5);
-					break;
-
-				case 9:
-					phone.Text = "+0 (0" + phoneString.Substring(0, 2) + ") " + phoneString.Substring(2, 2) + "-" + phoneString.Substring(4, 2) + "-" + phoneString.Substring(6);
-					break;
-
-				case 10:
-					phone.Text = "+0 (" + phoneString.Substring(0, 3) + ") " + phoneString.Substring(3, 2) + "-" + phoneString.Substring(5, 2) + "-" + phoneString.Substring(7);
-					break;
-
-				case 11:
-					phone.Text = "+" + phoneString.Substring(0, 1) + " (" + phoneString.Substring(1, 3) + ") " + phoneString.Substring(4, 2) + "-" + phoneString.Substring(5, 2) + "-" + phoneString.Substring(7);
-					break;
-			}
-        }
+		}
     }
 }
